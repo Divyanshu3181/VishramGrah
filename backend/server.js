f4e4ca38b01dfac8831a37482b75a522f0e297c8
Divyanshu3181 <sharmadivyanshu9752@gmail.com>
2024-11-13 16:48:13 +0530

    correct the db file location

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const dbConfig = require("../db");
+const dbConfig = require("./db");
 const roomsRoute = require('./routes/roomsRoute');
 
 app.use('/api/rooms', roomsRoute);
